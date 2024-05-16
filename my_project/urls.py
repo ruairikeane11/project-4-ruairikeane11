c66beb6a5297c7b947bc031ab3578f2693ef7181
ruairi keane <ruairi.rk@icloud.com>
2024-05-16 11:06:34 +0000

    Added authentication

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("contact/", include("contact.urls"), name="contact-urls"),
+    path("accounts/", include("allauth.urls")),
     path("", include("home.urls"), name="book-urls"),
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
