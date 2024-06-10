65f9730738919dd61011df77e24734b10387ff36
ruairi keane <ruairi.rk@icloud.com>
2024-06-10 13:29:51 +0000

    Add delete_conatct view to allow user to delete instance of ContactForm from database

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.contact_us, name='contact'),
+    path('contact/delete/<int:contact_id>/', views.contact_delete , name= 'contact_delete')
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
 from .forms import ContactForm
+from django.contrib import messages
  
 
 def contact_us(request):
@@ -8,22 +9,31 @@ def contact_us(request):
     Renders the contact page with ContactForm
     """
 
-    submitted_content = None
-
-
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             contact = form.save()
-            submitted_content = contact.content
-            form = ContactForm()
+            messages.success(request, "Your message has been submitted.")
     else:
         form = ContactForm()
+
+    submitted_contacts = Contact.objects.all().order_by('-created_on')
 
     return render (
         request,
         "contact/contact.html",
-        {"form":form, 'submitted_content': submitted_content},
+        {"form":form,
+        'submitted_contacts': submitted_contacts},
     )
         
 
+
+
+def contact_delete(request, contact_id):
+    """
+    Deletes instance of ContactForm from database
+    """
+    contact = get_object_or_404(Contact, id=contact_id)
+    contact.delete()
+    messages.success(request, "Contact form successfully deleted")
+    return redirect('contact')
